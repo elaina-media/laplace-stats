aa0a2e75783b74082662c248b1ca7c5d71f3a8b0
mikoto <mikoto@ungine.cn>
2024-04-14 02:40:09 +0800

    :rocket: 新增mapper扫描器

--- src/main/java/net/mikoto/laplace/statistics/LaplaceStatisticsApplication.java
+++ src/main/java/net/mikoto/laplace/statistics/LaplaceStatisticsApplication.java
@@ -1,9 +1,11 @@
 package net.mikoto.laplace.statistics;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("net.mikoto.laplace.statistics.mapper")
 public class LaplaceStatisticsApplication {
 
     public static void main(String[] args) {
